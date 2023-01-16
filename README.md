af733b9ef11bedc8458dfd450ae1cc532a4380a2
bolajipemipo <bolajipemipo@gmail.com>
2023-01-16 21:30:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# list-item-highlight
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ke7n7n)
---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component, VERSION } from '@angular/core';
+import { SfNode } from './sf-node';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  allNodes: SfNode[] = [
+    new SfNode('1', 'First Description'),
+    new SfNode('2', 'Second Description'),
+    new SfNode('3', 'Third Description'),
+    new SfNode('4', 'Fourth Description'),
+  ];
+
+  nodesProcessed: SfNode[] = [];
+
+  startingNode?: SfNode | null;
+  endingNode?: SfNode | null;
+
+  onmousedown($event: MouseEvent, node: SfNode) {
+    this.startingNode = node;
+    this.endingNode = null;
+    this.nodesProcessed = [node];
+  }
+
+  onmouseover($event: MouseEvent, node: SfNode) {
+    if (!this.startingNode) {
+      return;
+    }
+
+    const onTheList = this.isANodeToProcess(node);
+
+    if (onTheList) {
+      const nextNode = this.findNextNodeToProcess(node);
+      if (nextNode) {
+        this.removeNodeFromProcessing(nextNode);
+      }
+    }
+
+    if (!onTheList) {
+      this.nodesProcessed.push(node);
+    }
+  }
+
+  onmouseup($event: MouseEvent, node: SfNode) {
+    if (!this.startingNode) {
+      return;
+    }
+
+    if (this.nodesProcessed.length === 1) {
+      this.removeNodeFromProcessing(node);
+    }
+
+    this.endingNode = node;
+    this.startingNode = null;
+  }
+
+  private isANodeToProcess(node: SfNode): SfNode | undefined {
+    return this.nodesProcessed.find((lNode) => lNode.id === node.id);
+  }
+
+  private removeNodeFromProcessing(node: SfNode) {
+    this.nodesProcessed.splice(this.nodesProcessed.indexOf(node), 1);
+  }
+
+  private findNextNodeToProcess(node: SfNode): SfNode | undefined {
+    return this.nodesProcessed[this.nodesProcessed.indexOf(node) + 1];
+  }
+}
